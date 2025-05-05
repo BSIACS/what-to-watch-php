1a1a3bb542f890b9c72816db195f471314324313
BSI <bsi.acs14@gmail.com>
2025-05-05 19:29:52 +0300

    FIX: Исправить сидер БД

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    private readonly array $users;
+    private readonly array $roles;
+
+    function __construct()
+    {
+        $this->roles = [
+            'admin' => 'admin',
+            'moderator' => 'moderator',
+            'user' => 'user',
+        ];
+
+        $this->users = [
+            [
+                'name' => 'Пользователь',
+                'email' => 'user@anymail.ru',
+                'password' => '123456',
+                'role' => 'user',
+            ],
+            [
+                'name' => 'Администратор',
+                'email' => 'admin@anymail.ru',
+                'password' => '123456',
+                'role' => 'admin',
+            ],
+        ];
+    }
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->seedRoles();
+        $this->seedUsers();
+    }
+
+    private function seedRoles(): void {
+        $count = DB::table('roles')->get()->count();
+
+        if($count <= 0) {
+            foreach ($this->roles as $role) {
+                DB::table('roles')->insert([
+                    'id' => Str::uuid(),
+                    'name' => $role,
+                ]);
+            }
+        }
+    }
+
+    private function seedUsers(): void {
+        $count = DB::table('users')->get()->count();
+
+        if($count <= 0) {
+            foreach ($this->users as $user) {
+                $role = Role::query()->where('name', '=', $user['role'])->first();
+
+                User::query()->create([
+                    "id" => Str::uuid(),
+                    "name" => $user['name'],
+                    "email" => $user['email'],
+                    "password" => $user['password'],
+                    "role_id" => $role->id,
+                ]);
+            }
+        }
+    }
+}
